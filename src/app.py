4d1c5d7bfa1961ef422b86c08d1b0f5bc3ca8ceb
Lina Anaya <142395141+linama2593@users.noreply.github.com>
2023-10-18 08:27:20 +0000

    spotify API request processed

--- src/app.py
+++ src/app.py
@@ -1,15 +1,64 @@
+#pip install spotipy
+#pip install python-dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 import os
-from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
 import pandas as pd
 from dotenv import load_dotenv
+import numpy as np
 
-# load the .env file variables
+############# loading  the .env file variables################
 load_dotenv()
+client_id = os.getenv("SPOTIPY_CLIENT_ID")
+client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
+#################### Creating the Spotify client ####################
+con = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+################### Making the API request #######################
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+#Getting artist ID
+fatima_id='6jZSXmTCxZhFfYELtp78Ci'
+
+#importing songs
+output=con.artist_top_tracks(fatima_id)
+
+songs=[]
+if output:
+    for track in output['tracks']:
+        tracks = {}
+        tracks['name']=track['name']
+        tracks['popularity']=track['popularity']
+        tracks['duration']=track['duration_ms']/60000
+        songs.append(tracks)
+
+############### Creating DataFrame #############
+songs_df=pd.DataFrame(songs)
+songs_df
+
+
+############# Plotting scatter plot ###########
+
+# Create the trendline
+coefficients = np.polyfit(songs_df['duration'], songs_df['popularity'], 1)  # Fit a first-degree (linear) polynomial
+trendline = np.poly1d(coefficients)
+y_fit = trendline(songs_df['duration'])
+
+#Creating plot
+plt.scatter(songs_df['duration'], songs_df['popularity'])
+plt.plot(songs_df['duration'], y_fit, linestyle='--', color='red')
+plt.title('Relationship between duration and popularity')
+plt.xlabel('Song duration in minutes')
+plt.ylabel('Popularity ranking')
+plt.show()
+
+print("""
+Conclusion: There is a possitive association between
+duration and popularity, based on the trendline.
+However, given the few data points available, it is not possible to
+determine whether this association is statistically significant.
+Even if this association was statistically significant, correlation
+is not causation. 
+      """)
